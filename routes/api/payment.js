e108713a1af1f3f8fd7a8af380f09ddf370c0847
Jillo Wario <willywario0@gmail.com>
2025-05-08 12:05:47 +0300

    STK push

--- routes/api/payment.js
+++ routes/api/payment.js
@@ -5,24 +5,58 @@ const upload = multer({ storage: storage });
 const router = express.Router();
 const { v1: uuidv1, v4: uuidv4 } = require("uuid");
 const MONGODB_URL = process.env.MONGODB_URL;
-const axios = require('axios');
+const axios = require("axios");
 // const db = require("monk")(MONGODB_URL);
 const db = require("monk")(process.env.MONGODB_URL);
 // const db = require("monk")("mongodb://localhost:27017/inventory-management");
+const CurrencyConverter = require("currency-converter-lt");
 
+const cheerio = require("cheerio");
+
+// Function to scrape exchange rate
+async function getUsdToKesRate() {
+  try {
+    const { data } = await axios.get(
+      "https://www.google.com/search?q=USD+to+KES"
+    );
+    const $ = cheerio.load(data);
+
+    // Extracting exchange rate from the page
+    const rate = $("div[data-precision]").first().text();
+    console.log("rate", rate);
+    if (rate) {
+      return parseFloat(rate);
+    } else {
+      throw new Error("Unable to find exchange rate on the page");
+    }
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error.message);
+    throw error;
+  }
+}
+
+const convertUsdToKes = async (usdAmount) => {
+  console.log(await getUsdToKesRate());
+};
+
+const roundUpAmount = (amount) => {
+  return Number(Math.ceil(amount));
+};
 
 // Initiate M-Pesa Payment via Paystack
 router.post("/initializeMpesaStkPush", async (req, res) => {
   const { email = "live@gmail.com", amount, phone } = req.body;
+  const convertedAmount = roundUpAmount(amount);
 
-  console.log("running");
+  console.log("final amount", convertedAmount);
 
   try {
     const response = await axios.post(
       "https://api.paystack.co/charge",
       {
         email,
-        amount,
+        // amount: convertedAmount * 100,
+        amount: 1 * 100,
         currency: "KES",
         mobile_money: {
           phone,
