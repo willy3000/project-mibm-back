1fcf16a8b2d2619cc4ff3411b1ba7d6add4c32f5
Jillo Wario <willywario0@gmail.com>
2025-05-08 07:00:00 +0300

    added live frontend url to cors policy

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ var cors = require("cors");
 const corsOptions = {
   // origin: "http://localhost:3000", // Replace with your frontend domain
   // origin: "http://192.168.100.6:3000", // Replace with your frontend domain,
-  origin: ['http://localhost:3000', '"http://192.168.100.6:3000"'],
+  origin: [
+    "http://localhost:3000",
+    "http://192.168.100.6:3000",
+    "https://inventory-project-mibm.netlify.app/authentication#",
+  ],
   // origin: "*", // Replace with your frontend domain
   methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
   credentials: true, // If you need to send cookies or HTTP authentication
